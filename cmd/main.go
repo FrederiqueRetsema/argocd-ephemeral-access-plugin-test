07902f8ca202dd7ca63e89154445807898942e9a
Frederique <frederique.retsema@conclusionxforce.nl>
2025-05-12 17:03:07 +0200

    20250512

--- cmd/main.go
+++ cmd/main.go
@@ -336,7 +336,7 @@ func (p *ServiceNowPlugin) GrantAccess(ar *api.AccessRequest, app *argocd.Applic
 	jsonAr, _ := json.Marshal(ar)
 	p.Logger.Debug(string(jsonAr))
 
-	grantedAccessTextUI := fmt.Sprintf("Granted access: change __%s__ (%s), until __%s (UTC)__", changeNumber, changeShortDescription, changeEndDate)
+	grantedAccessTextUI := fmt.Sprintf("Granted access: change __%s__ (%s), until __%s (%s min)__", changeNumber, changeShortDescription, changeEndDate, string(changeRemainingTime))
 	p.Logger.Debug(grantedAccessTextUI)
 	return &plugin.GrantResponse{
 		Status: plugin.GrantStatusGranted,
