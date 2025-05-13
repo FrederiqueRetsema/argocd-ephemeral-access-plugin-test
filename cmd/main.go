3aac10f63e76bac2724b5664183ef403716001fd
Frederique <frederique.retsema@conclusionxforce.nl>
2025-05-13 08:37:52 +0200

    20250513

--- cmd/main.go
+++ cmd/main.go
@@ -327,6 +327,8 @@ func (p *ServiceNowPlugin) GrantAccess(ar *api.AccessRequest, app *argocd.Applic
 		}
 		if validChange || len(changes) < sysparm_limit {
 			break
+		} else {
+			changes, sysparm_offset := p.getChanges(username, password, ciName, sysparm_offset)
 		}
 	}
 	
