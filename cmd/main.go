77ebad5a71e269c0ca39dbf1d18cd059ac85f441
Frederique <frederique.retsema@conclusionxforce.nl>
2025-05-13 18:35:49 +0200

    20250513

--- cmd/main.go
+++ cmd/main.go
@@ -435,7 +435,7 @@ func (p *ServiceNowPlugin) GrantAccess(ar *api.AccessRequest, app *argocd.Applic
 
 	arDurationString := arDuration.String()
 	var arDurationTime time.Duration
-	arDurationTime.UnmarchalText([]byte(arDurationString))
+	arDurationTime.UnmarshalText([]byte(arDurationString))
 
 	if arDurationTime > changeRemainingTime {  
 		ar.Spec.Duration.Duration = changeRemainingTime
