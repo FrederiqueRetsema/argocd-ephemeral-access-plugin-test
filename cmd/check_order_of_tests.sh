5d13f73556b4eb8b23771694cf0919880ae11308
Frederique <frederique.retsema@conclusionxforce.nl>
2025-05-24 18:30:34 +0200

    20250524

--- cmd/check_order_of_tests.sh
+++ cmd/check_order_of_tests.sh
@@ -1,26 +1,36 @@
-GREP_RESULT=$(grep func main.go| awk '{print $4}'|awk -F'(' '{print $1}')
+PROGRAM_FILE=./main.go
+TEST_FILE=./service-now-plugin_test.go
+
+
+GREP_RESULT=$(grep func ${PROGRAM_FILE}| awk '{print $4}'|awk -F'(' '{print $1}')
 
 PREV_LINE_NUMBER=0
 PREV_FUNCTION=""
-ERROR_FOUND=1
+ERROR_FOUND=0
 
-for FUNCTION in $GREP_RESULT
+echo "Order in ${PROGRAM_FILE}:"
+echo ""
+
+for FUNCTION in ${GREP_RESULT}
 do
-	echo $FUNCTION
+	echo ${FUNCTION}
 
-	LINE_NUMBER=$(grep -n -i $FUNCTION ./service-now-plugin_test.go | grep func | head -n 1 | awk -F':' '{print $1}')
+	LINE_NUMBER=$(grep -n -i ${FUNCTION} ${TEST_FILE} | grep func | head -n 1 | awk -F':' '{print $1}')
 	if test ${LINE_NUMBER} -lt ${PREV_LINE_NUMBER}
 	then
 		echo "${FUNCTION} is after ${PREV_FUNCTION} in main.go, it is before ${PREV_FUNCTION} in testset (${LINE_NUMBER}, ${PREV_LINE_NUMBER})"
 		ERROR_FOUND=1
 	fi
 
-	PREV_LINE_NUMBER=$LINE_NUMBER
+	PREV_LINE_NUMBER=${LINE_NUMBER}
 	PREV_FUNCTION=${FUNCTION}
 done
 
-if test ${ERROR_FOUND} -eq 1
+echo ""
+if test ${ERROR_FOUND} -eq 0
 then
+	echo "Order in ${PROGRAM_FILE} is the same as in ${TEST_FILE}"
+else
 	echo "Errors found, please fix them"
 	exit 1
 fi
